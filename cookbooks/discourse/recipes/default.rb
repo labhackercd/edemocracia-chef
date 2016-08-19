f3ac313bc2d1c5c4f59f9d1efada9b24de6e919c
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-08-19 14:52:21 -0300

    Finished discourse recipe
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- cookbooks/discourse/recipes/default.rb
+++ cookbooks/discourse/recipes/default.rb
@@ -1,10 +1,16 @@
 dependencies = ['epel-release', 'ruby', 'ruby-devel', 'nginx',
                 'postgresql', 'postgresql-server', 'postgresql-contrib', 'redis',
                 'ImageMagick', 'libxml2', 'gifsicle', 'libpqxx-devel', 'make',
-                'libjpeg', 'policycoreutils-python', 'gcc-c++']
+                'libjpeg', 'policycoreutils-python', 'gcc-c++', 'optipng', 'jhead',
+                'jpegoptim', 'libjpeg-turbo-utils', 'nodejs', 'npm']
 
 dependencies.each do |package_name|
   package "#{package_name}"
+end
+
+execute 'install:svgo' do
+  command 'npm install -g svgo'
+  action :run
 end
 
 execute 'gem install bundler'
@@ -66,6 +72,20 @@ service 'redis' do
   action [:start, :enable]
 end
 
+file "#{node['config']['discourse']['dir']}/sso_secret.key" do
+  content "#{([*('A'..'Z'),*('0'..'9'),*('a'..'z')]-%w(0 1 I O)).sample(100).join}"
+  action :create_if_missing
+  owner "#{node['config']['system']['user']}"
+  group "discourse"
+  mode '0755'
+end
+
+template "#{node['config']['discourse']['dir']}/config/site_settings.yml" do
+  user node['config']['system']['user']
+  group 'discourse'
+  mode '0644'
+end
+
 execute '/usr/local/bin/bundle exec rake db:migrate' do
   user node['config']['system']['user']
   group 'discourse'
@@ -74,29 +94,36 @@ execute '/usr/local/bin/bundle exec rake db:migrate' do
   action :run
 end
 
-execute '/usr/local/bin/bundle exec rake assets:precompile' do
-  user node['config']['system']['user']
-  group 'discourse'
-  environment ({"RAILS_ENV" => "production"})
-  cwd node['config']['discourse']['dir']
-  action :run
-end
+# execute '/usr/local/bin/bundle exec rake assets:precompile' do
+#   user node['config']['system']['user']
+#   group 'discourse'
+#   environment ({"RAILS_ENV" => "production"})
+#   cwd node['config']['discourse']['dir']
+#   action :run
+# end
 
 template '/etc/systemd/system/discourse.service' do
   owner 'root'
   group 'root'
   mode '0644'
+  notifies :restart, 'service[discourse]'
 end
 
 template '/etc/systemd/system/discourse-sidekiq.service' do
   owner 'root'
   group 'root'
   mode '0644'
+  notifies :restart, 'service[discourse-sidekiq]'
 end
 
 execute 'systemctl daemon-reload'
 
 service 'discourse' do
   supports :status => true, :restart => true, :reload => true
-  action [:start, :enable]
+  action [:restart, :enable]
+end
+
+service 'discourse-sidekiq' do
+  supports :status => true, :restart => true, :reload => true
+  action [:restart, :enable]
 end
